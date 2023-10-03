759ec0aa90a38fa42ed68dde9a8821ad2787aeb6
Pankaj Kumar <pankajs2167@gmal.com>
2023-10-03 21:13:40 +0530

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorhandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken';
 
 export  const signup = async (req,res,next) => {
     const{username,email,password}=req.body;
@@ -12,3 +14,27 @@ export  const signup = async (req,res,next) => {
         next(error);
     }
 };
+
+
+export const signin =async (req,res,next)=>{
+    const{email,password}=req.body;
+    try{
+
+        const validUser =await User.findOne({email});
+        if(!validUser){
+            return next(errorhandler(404,'User not found!'));
+        };
+
+        const validPassword =bcryptjs.hashSync(password,validUser.password);
+        if(!validPassword) {
+            return next (errorhandler(401,'Wrong cridential!'));
+        };
+        const token =jwt.sign({id:validUser._id},process.env.JWT_SECRET)
+        const { password:pass, ...rest}=validUser._doc;
+
+        res.cookie('access_token',token,{httpOnly:true}).status(200).json(rest);
+
+    }catch(error){
+        next(error);
+    }
+}
