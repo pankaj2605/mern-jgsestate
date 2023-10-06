ea0043334e5064d91ce01b2eb4b9b27e8a7e06b6
Pankaj Kumar <pankajs2167@gmal.com>
2023-10-06 19:09:37 +0530

    create user update api

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import { test } from '../controllers/user.controller.js';
+import { verifyToken } from '../utils/verifyUser.js';
+import { updateUser } from '../controllers/user.controller.js';
+
 
 const router=express.Router();
 
 
 router.get('/test', test);
+router.post('/update/:id',verifyToken, updateUser)
 
 
 export default router;
