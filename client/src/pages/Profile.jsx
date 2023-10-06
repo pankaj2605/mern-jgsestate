35d7348fbfb7ea9e75cba3d13a44b3c2199d3f58
Pankaj Kumar <pankajs2167@gmal.com>
2023-10-06 12:04:52 +0530

    complete image upload functionallity

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,13 +1,49 @@
-import React from 'react';
+import React,{useRef,useState,useEffect} from 'react';
 import { useSelector } from 'react-redux';
+import {getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/storage';
+import { app } from '../firebase';
 
 export default function Profile() {
+  const fileRef =useRef(null)
   const {currentUser} = useSelector((state)=>state.user)
+  const [file,setFile]=useState(undefined);
+  const [filePerc,setFilePerc]=useState(0);
+  const [fileUploadError,setFileUploadError]=useState(false);
+  const [formData,setFormData]= useState({});
+  
+
+  useEffect(()=>{
+    if(file){
+      handlerFileUpload(file);
+    }
+
+  },[file]);
+  const handlerFileUpload=(file)=>{
+    const storage= getStorage(app);
+    const fileName= new Date().getTime()+file.name;
+    const storageRef=ref(storage,fileName);
+    const upLoadTask=uploadBytesResumable(storageRef,file);
+    upLoadTask.on('state_changed',(snapshot)=>{
+      const progess=(snapshot.bytesTransferred/snapshot.totalBytes)*100;
+      setFilePerc(Math.round(progess));
+    },(error)=>{
+      setFileUploadError(true);
+    }, ()=>{
+      getDownloadURL(upLoadTask.snapshot.ref).then((DownloadURL)=>{
+        setFormData({...formData,avatar:DownloadURL});
+      });
+    });
+  }
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
       <form className='flex flex-col gap-4'>
-        <img src={currentUser.avatar} alt="profile" className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2 '/>
+      <input onChange={(e)=>setFile(e.target.files[0])} type="file" ref={fileRef} hidden accept='image/*'/>
+        <img onClick={()=>fileRef.current.click()} src={formData.avatar || currentUser.avatar} alt="profile" className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2 '/>
+        <p className='text-sm self-center' >               {fileUploadError ? 
+                            (<span className='text-red-700'>Error Image Upload ( Image must be less than 2mb ) </span>):
+                            filePerc>0 && filePerc<100 ?  (<span className='text-slate-700'>{`uploading ${filePerc}%`}</span>) : filePerc === 100 ? (<span className='text-green-700'>Image Succesfully Uploaded!</span>):""
+                            }</p>
         <input type="text" placeholder="username" id='username' className='border p-3 rounded-lg'/>
         <input type="email" placeholder="email" id='email' className='border p-3 rounded-lg'/>
         <input type="rext" placeholder="password" id='password' className='border p-3 rounded-lg'/>
