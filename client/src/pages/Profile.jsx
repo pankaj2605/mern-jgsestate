ddc5e24c1a807d0a3d16a9939cf809c08316905d
Pankaj Kumar <pankajs2167@gmal.com>
2023-10-09 08:02:21 +0530

    add delete user functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,7 +2,7 @@ import React,{useRef,useState,useEffect} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import {getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/storage';
 import { app } from '../firebase';
-import { updateUserStart,updateUserSuccess,updateUserFailure } from '../redux/user/userSlice';
+import { updateUserStart,updateUserSuccess,updateUserFailure,deleteUserStart,deleteUserSuccess,deleteUserFailure } from '../redux/user/userSlice';
 
 
 export default function Profile() {
@@ -49,10 +49,6 @@ export default function Profile() {
       dispatch(updateUserStart());
       const res=await fetch(`/api/user/update/${currentUser._id}`,{
         method:'POST',
-          headers:{
-            'Content-Type':'application/json',
-          },
-          body:JSON.stringify(formData),
 
       });
       const data=await res.json();
@@ -66,6 +62,29 @@ export default function Profile() {
         setUpdateSuccess(true);
     }catch(error){
       dispatch(updateUserFailure(error.message));
+    }
+  }
+
+  const handleDeleteUser= async ()=>{
+    try{
+      dispatch(deleteUserStart());
+      const res=await fetch(`/api/user/delete/${currentUser._id}`,{
+        method:'DELETE',
+        headers:{
+          'Content-Type':'application/json',
+        },
+      });
+      const data=await res.json();
+
+        if(data.success  === false){
+          dispatch(deleteUserFailure(data.message));
+          return;
+        };
+
+        dispatch(deleteUserSuccess(data));
+    }
+    catch(error){
+      dispatch(deleteUserFailure(error.message));
     }
   }
 
@@ -85,7 +104,7 @@ export default function Profile() {
         <button disabled={loading}className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disable:opacity-80'>{loading ? 'loading....' : 'Update'}</button>
       </form>
       <div className='flex justify-between mt-5'>
-        <span className='text-red-700 cursor-pointer'>Delete Account</span>
+        <span onClick={handleDeleteUser} className='text-red-700 cursor-pointer'>Delete Account</span>
         <span className='text-red-700 cursor-pointer'>Sign out</span>
       </div>
       <p className='text-red-700 mt-5'>{error?error:''}</p>
