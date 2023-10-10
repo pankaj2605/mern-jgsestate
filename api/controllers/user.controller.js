8b6989928cef1858fdb37e6ca53c7524849658a1
Pankaj Kumar <pankajs2167@gmal.com>
2023-10-10 11:13:24 +0530

    create git user listing api routes

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import { errorhandler } from "../utils/error.js";
 import bcryptjs from "bcryptjs";
 import User from "../models/user.model.js";
+import Listing from "../models/listing.model.js";
 
 export const test = (req,res)=>{
 
@@ -45,5 +46,17 @@ export const deleteUser = async (req,res,next)=>{
                 next(error)
 
         }
+
+}
+
+export const getUserListings = async (req,res,next)=>{
+        if(req.user.id !== req.params.id) return next(errorhandler(401,'You can only view your own listings!'))
+        try{
+                const listings = await Listing.find({userRef:req.params.id});
+                res.status(200).json(listings);
+        }catch(error){
+                next(error)
+        }
+
 
 }
