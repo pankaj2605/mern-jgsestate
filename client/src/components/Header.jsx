e2ce855cda6ab8ebfab56c935f102ecdef5875d6
Pankaj Kumar <pankajs2167@gmal.com>
2023-10-11 14:03:06 +0530

    update header search form functionallity

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState,useEffect } from 'react';
 import {FaSearch} from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import { Link,useNavigate } from 'react-router-dom';
 import {  useSelector } from 'react-redux';
 
 export default function Header() {
-    const {currentUser}= useSelector(state =>state.user)
+    const {currentUser}= useSelector(state =>state.user);
+    const [searchTerm,setSearchTerm]= useState('');
+    const navigate=useNavigate();
+
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+        const urlParams=new URLSearchParams(window.location.search);
+        urlParams.set('searchTerm',searchTerm);
+        const searchQuery=urlParams.toString();
+        console.log(searchQuery);
+        navigate(`/search?${searchQuery}`)
+    };
+
+    useEffect(()=>{
+        const urlParams =new URLSearchParams(location.search);
+        const searchTermFromUrl =urlParams.get('searchTerm');
+        if(searchTermFromUrl){
+            setSearchTerm(searchTermFromUrl);
+        }
+    },[location.search]);
+
   return (
     <header className='bg-slate-200 shadow-md'>
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
@@ -15,9 +35,9 @@ export default function Header() {
                 </h1>
             </Link>
             
-            <form className='bg-slate-100 p-3 rounded-lg flex items-center '>
-                <input type="text" placeholder="Search..." className='bg-transparent focus:outline-none w-24 sm:w-64'/>
-                <FaSearch className='text-slate-500'/>
+            <form onSubmit={handleSubmit} className='bg-slate-100 p-3 rounded-lg flex items-center '>
+                <input type="text" placeholder="Search..." value={searchTerm} onChange={(e)=>{setSearchTerm(e.target.value)}} className='bg-transparent focus:outline-none w-24 sm:w-64'/>
+                <button><FaSearch className='text-slate-500'/></button>
             </form>
             <ul className='flex gap-4'>
                 <Link to='/'>
