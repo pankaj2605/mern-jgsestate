0442e5f1892082a8abca9f86a27ed7efcd54aeb3
Pankaj Kumar <pankajs2167@gmal.com>
2023-10-12 13:06:09 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -5,6 +5,8 @@ import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
 import listingRouter from "./routes/listing.route.js";
+import path from 'path';
+
 
 dotenv.config();
 
@@ -16,6 +18,8 @@ mongoose.connect(process.env.MONGO).then(()=>{
             console.log(err)
         
             });
+
+    const __dirname=path.resolve();
             
         
 
@@ -35,6 +39,13 @@ app.listen(3000,()=>{
 app.use("/api/user",userRouter);
 app.use("/api/auth",authRouter);
 app.use("/api/listing",listingRouter);
+
+
+app.use(express.static(path.join(__dirname,'/client/dist')));
+
+app.get('*',(res,req)=>{
+    res.sendFile(path.join(__dirname,'client','dist','index.html'));
+})
 
 
 app.use((err,req,res,next)=>{
