df205b42f4b2effce66a0cb1cab03d6f0ba312d1
Pankaj Kumar <pankajs2167@gmal.com>
2023-10-18 08:19:00 +0530

    change postion of req and res

--- api/index.js
+++ api/index.js
@@ -43,7 +43,7 @@ app.use("/api/listing",listingRouter);
 
 app.use(express.static(path.join(__dirname,'/client/dist')));
 
-app.get('*',(res,req)=>{
+app.get('*',(req,res)=>{
     res.sendFile(path.join(__dirname,'client','dist','index.html'));
 })
 
